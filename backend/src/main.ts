6e5d1ba8b7421029e5186379ea25807c1ae5cc19
Magdy <momagdy@grouphc.net>
2023-01-01 22:14:42 +0200

    Checkout For The Dev Branch

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,7 +10,7 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
-console.log('UdaPeople Is The Best');
+console.log('UdaPeople Checkout');
 async function bootstrap() {
   const logger = new AppLogger();
   logger.info(`NodeJs Version ${process.version}`);
